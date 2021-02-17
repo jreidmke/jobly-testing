391fe9b2cce11bde01922a9d3cb359e0c433d8f9
James Reid <jreidmke@gmail.com>
2021-02-17 08:34:37 -0600

    cleaned up reg test

--- selenium_tests/register/registerSuccess.js
+++ selenium_tests/register/registerSuccess.js
@@ -6,7 +6,7 @@ let driver = new webdriver.Builder().forBrowser("chrome").build();
 async function registerSuccess() {
     try {
         await driver.get(`${baseUrl}signup`);
-        await driver.findElement(webdriver.By.name("username")).sendKeys("jreidmke");
+        await driver.findElement(webdriver.By.name("username")).sendKeys((Math.random() * 1000).toString());
         await driver.findElement(webdriver.By.name("password")).sendKeys("password");
         await driver.findElement(webdriver.By.name("firstName")).sendKeys("James");
         await driver.findElement(webdriver.By.name("lastName")).sendKeys("Reid");
