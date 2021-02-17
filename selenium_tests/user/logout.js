736b68d75f9784be097bc5733bac027db6f9a122
James Reid <jreidmke@gmail.com>
2021-02-17 09:24:18 -0600

    new folder name

---
+++ selenium_tests/user/logout.js
@@ -0,0 +1,29 @@
+const webdriver = require('selenium-webdriver');
+require("colors");
+const baseUrl = 'http://localhost:3000/';
+let driver = new webdriver.Builder().forBrowser("chrome").build();
+
+async function loginSuccess() {
+    try {
+        await driver.get(`${baseUrl}login`);
+        await driver.findElement(webdriver.By.id("usernameInput")).sendKeys("testuser");
+        await driver.findElement(webdriver.By.id("passwordInput")).sendKeys("password\n");
+        await driver.sleep(2000);
+        await driver.findElement(webdriver.By.id("logout-link")).click();
+        await driver.sleep(500);
+        const title = await driver.getTitle();
+        console.log(title);
+        if(title.includes("Homepage")) {
+            console.log('Logout Success Test Passed'.green);
+        } else {
+            console.log('Logout Success Test Failed'.red);
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        if(driver) {
+            (await driver).quit()
+        }
+    }
+};
+loginSuccess();
