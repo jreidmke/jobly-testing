363b0da71d09818e2148d7e23e1a4692a5fa2193
James Reid <jreidmke@gmail.com>
2021-02-17 08:34:19 -0600

    new company view test

--- selenium_tests/companies/companyDetailsView.js
+++ selenium_tests/companies/companyDetailsView.js
@@ -3,16 +3,16 @@ require("colors");
 const baseUrl = 'http://localhost:3000/';
 let driver = new webdriver.Builder().forBrowser("chrome").build();
 
-async function companyListSearch() {
+async function companyDetailsView() {
     try {
         await driver.get(`${baseUrl}login`);
         await driver.findElement(webdriver.By.id("usernameInput")).sendKeys("testuser");
         await driver.findElement(webdriver.By.id("passwordInput")).sendKeys("password\n");
         await driver.sleep(2000);
-        await driver.findElement(webdriver.By.name("searchTerm")).sendKeys("Ayala\n");
+        await driver.findElement(webdriver.By.xpath("//*[text()='Anderson, Arias and Morrow']")).click();
         await driver.sleep(500);
-        const ayalaText = await driver.findElement(webdriver.By.xpath("//*[text()='Ayala-Buchanan']")).getText();
-        if(ayalaText) {
+        const jobListingText = await driver.findElement(webdriver.By.xpath("//*[text()='Technical brewer']")).getText();
+        if(jobListingText) {
             console.log("Company List Search Test Passed".green);
         } else {
             console.log("Company List Search Test Failed".red);
@@ -26,4 +26,4 @@ async function companyListSearch() {
     }
 };
 
-companyListSearch();
+companyDetailsView();
